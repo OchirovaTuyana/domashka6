d039067717d15286c5536ddd413f67f0372eb104
Tuyana <tuyaochirova98@gmail.com>
2022-11-29 19:46:39 +0800

    zadacha 43

--- zadacha43/Program.cs
+++ zadacha43/Program.cs
@@ -16,7 +16,6 @@ double pointOfIntersection (double b1, double k1, double b2, double k2)
 {         
     double x = (b2 - b1) / (k1 - k2);
     double y = k1 * x + b1;
-    // int y = k2 * x + b2;
     Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
     return x;
 }
