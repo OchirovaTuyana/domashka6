3003218be459376896286f3503b717e4d8494b81
Tuyana <tuyaochirova98@gmail.com>
2022-11-29 19:38:20 +0800

    zadacha 43

--- zadacha43/Program.cs
+++ zadacha43/Program.cs
@@ -12,7 +12,13 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число k2: ");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (-b2 + b1)/(-k1 + k2);
-double y = k2 * x + b2;
+double pointOfIntersection (double b1, double k1, double b2, double k2)
+{         
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    // int y = k2 * x + b2;
+    Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
+    return x;
+}
 
-Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
+double result = pointOfIntersection (b1, k1, b2, k2);
